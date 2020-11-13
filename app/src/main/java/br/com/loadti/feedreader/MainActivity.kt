687d3191f2bf56b04c5f88d91ecad85166c7c57a
williandoamor <willsazon@gmail.com>
2020-11-13 11:09:11 -0300

    Organizando o Projeto.

---
+++ app/src/main/java/br/com/loadti/feedreader/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.loadti.feedreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
